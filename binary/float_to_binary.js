fd08b35e919f1b4a5f69f60238cd6f09ccb5bd0a
General Account <robo328a@dj2.(none)>
2014-06-13 15:37:37 -0400

    wheee

--- binary/float_to_binary.js
+++ binary/float_to_binary.js
@@ -1,44 +1,72 @@
 /* converts decimal to binary string */
+
+var toBinary_memo = {};
+
 function toBinary(num) {
-  var num = num;
-  var bin = '';
-  var pow = 7;
-  while (pow >= 0) {
-    if (Math.pow(2,pow) > num) {
-      bin = bin + '0';
-      pow = pow - 1;
+  if (!(toBinary_memo[num])) {
+    var to_go = num;
+    var bin = '';
+    var pow = 7;
+    while (pow >= 0) {
+      if (Math.pow(2,pow) > to_go) {
+        bin = bin + '0';
+        pow = pow - 1;
+      }
+      else {
+        bin = bin + '1';
+        to_go = to_go - Math.pow(2,pow);
+        pow = pow - 1;
+      }
     }
-    else {
-      bin = bin + '1';
-      num = num - Math.pow(2,pow);
-      pow = pow - 1;
-    }
+    toBinary_memo[num] = bin;
+  }
+  else {
+    var bin = toBinary_memo[num];
   }
   return bin;
 }
 
 /* converts binary string to decimal */
-function binaryToDecimal(bin) {
-  var tot = 0;
-  var pow = bin.length - 1;
-  var bin = bin;
-  while (bin.length > 0) {
-    if (bin[0] === '1') {
-      tot += Math.pow(2,pow);
-    }
-    pow -= 1;
-    bin = bin.slice(1);
-  }
-  return tot;
+
+var binaryToDecimal_memo = {};
+
+function lookup(bin) {
+  return binaryToDecimal_memo[bin];
 }
 
-var chars = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+function is_it_safe(bin) {
+  return !(binaryToDecimal_memo[bin]);
+}
 
-/* max-255 decimal to hex converter */
+function binaryToDecimal(bin) {
+ if (is_it_safe(bin)) {
+    var tot = 0;
+    var pow = bin.length - 1;
+    var to_go = bin;
+    while (to_go.length > 0) {
+      if (to_go[0] === '1') {
+        tot += Math.pow(2,pow);
+      }
+      pow -= 1;
+      to_go = to_go.slice(1);
+    }
+    binaryToDecimal_memo[bin] = tot;
+  }
+  return lookup(bin);
+}
+
+var chars = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+
+/* decimal to hex converter */
 function toHex(num) {
-  var num = num;
-  var remainder = Math.floor(num / 16);
-  return (chars[(num-remainder)/16] + chars[remainder]);
+  function helper(num,pow) {
+    if (pow === 0) {return chars[num]}
+    var remainder = num % Math.pow(16,pow);
+    return chars[(num-remainder)/Math.pow(16,pow)] + helper(remainder,pow-1)
+  }
+  var maxpow = 1;
+  while (Math.pow(16,maxpow) <= num) {maxpow += 1}
+  return helper(num, maxpow - 1);
 }
 
 /* converts a byte of binary to hexadecimal */
@@ -48,14 +76,9 @@ function binToHex(str) {
 
 /*returns array with elements corresponding to r,g,b */
 function hexToRGB(str) {
+  var str = str + ''
   var rgb = [str.slice(0,2),str.slice(2,4),str.slice(4)];
-  console.log(rgb);
-  var catcher = "wut";
-  var rye = ["wat"];
-  var newRGB = ['hai world'];
-  console.log(catcher);
-  console.log(rye);
-  console.log(newRGB);
+  var newRGB = [];
   for (var i=0; i < rgb.length; i++) {
     newRGB[i] = (chars.indexOf(rgb[i][0]))*16 +
                 (chars.indexOf(rgb[i][1]));
@@ -63,34 +86,58 @@ function hexToRGB(str) {
   return newRGB;
 }
 
-/*converts array of 4 numbers to decimal representation */
-function f32ToDecimal(nums) {
-  function decodeSig(str) {
-    var addable = 1;
-    var total = 0;
-    var i = 0;
-    while (i < str.length) {
-      if (str[i]==='1') {
-        total += addable;
-      }
-      i += 1;
-      addable /= 2;
+//var numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
+//var sig_stuff = [Math.pow(2,-i) for (i of numbers)]
+
+//var sigmemarr = [{range: numbers.slice(0,4)},
+//                 {range: numbers.slice(4,8)},
+//                 {range: numbers.slice(8,12)},
+//                 {range: numbers.slice(12,16)},
+//                 {range: numbers.slice(16,20)},
+//                 {range: numbers.slice(20)}]
+
+function decodeSig(str) {
+//  var total = 0;
+//  var strarr = [str.slice(0,4),str.slice(4,8),str.slice(8,12),
+//                str.slice(12,16),str.slice(16,20),str.slice(20)];
+// for (var i = 0; i < 6; i++) {
+//    var subtotal = 0;
+//    if (sigmemarr[i][strarr[i]] !== undefined) {
+//      total += sigmemarr[i][strarr[i]];
+//    } else {
+//      console.log('aw man, i gotta do work');
+//      for (var j = 0; j < 4; j++) {
+//        if (strarr[i][j] === 1) {
+//          subtotal += sig_stuff[i*4 + j];
+//        }
+//      }
+//      sigmemarr[i][strarr[i]] = subtotal;
+//    }
+//  }
+  var addable = 1;
+  var total = 0;
+  var i = 0;
+  while (i < str.length) {
+    if (str[i]==='1') {
+      total += addable;
     }
-    return total;
+    i += 1;
+    addable /= 2;
   }
-  var byte78 = toBinary(nums[0]);
+  return total;
+}
+
+/*converts array of 4 numbers to decimal representation (little-endian) */
+function f32ToDecimal(nums) {
+  var byte87 = toBinary(nums[0]);
   var byte65 = toBinary(nums[1]);
   var byte43 = toBinary(nums[2]);
   var byte21 = toBinary(nums[3]);
   var sign = byte21[0];
-  console.log('sign: ' + sign);
   var exp  = byte21.slice(1) + byte43[0];
-  var sig  = '1' + byte43.slice(1) + byte65 + byte78;
-  console.log('sig: ' + sig);
-  exp = binaryToDecimal(exp) - 127;
-  console.log('decode exp: ' + exp);
+  var sig  = '1' + byte43.slice(1) + byte65 + byte87
+  exp = parseInt(exp,2) - 127;
   sig = decodeSig(sig);
-  console.log('decoded sig: ' + sig);
   if (sign === '0') {
     var ans = sig * Math.pow(2,exp);
   }
@@ -100,7 +147,7 @@ function f32ToDecimal(nums) {
   return ans;
 }
 
-/* converts array of 4 numbers to corresponding RGB values */
-function f32ToArray(nums) {
+/* converts array of 4 numbers to corresponding RGB values (little-endian) */
+function f32ToRGB(nums) {
   return (hexToRGB(toHex(f32ToDecimal(nums))));
 }
